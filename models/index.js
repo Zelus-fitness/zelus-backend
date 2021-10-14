7712a09690bb4e31234ac601dd0953765950c7ff
caesar-chin <56019561+caesar-chin@users.noreply.github.com>
2021-10-14 15:47:07 -0400

    please work ssl for gods sake

--- models/index.js
+++ models/index.js
@@ -7,6 +7,12 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   port: dbConfig.PORT,
   dialect: dbConfig.dialect,
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
