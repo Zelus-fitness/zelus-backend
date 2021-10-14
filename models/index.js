e4cd465558e9b4f5d185acc0e24bed3db080e7b0
caesar-chin <56019561+caesar-chin@users.noreply.github.com>
2021-10-14 15:44:13 -0400

    ssl remove

--- models/index.js
+++ models/index.js
@@ -7,9 +7,6 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   port: dbConfig.PORT,
   dialect: dbConfig.dialect,
-  "dialectOptions": {
-    "ssl": true
-  },
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
