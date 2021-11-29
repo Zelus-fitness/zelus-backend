7238e4afd365c27a68b6fa1fc8120d01718b1b04
caesar-chin <56019561+caesar-chin@users.noreply.github.com>
2021-11-28 20:54:41 -0500

    added category for exercises

--- models/exercise.model.js
+++ models/exercise.model.js
@@ -24,6 +24,10 @@ module.exports = (sequelize, Sequelize) => {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
     },
     { timestamps: false }
   );
